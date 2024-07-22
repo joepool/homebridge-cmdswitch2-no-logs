970871962317d1cb79e3b421cbe8e2344a5dc8f1
joepool <65784403+joepool@users.noreply.github.com>
2024-07-22 18:10:56 +0100

    0.4.0 beta to fix homebridge V2 issues

--- index.js
+++ index.js
@@ -24,61 +24,40 @@ function cmdSwitchPlatform(log, config, api) {
   }
 }
 
-// Method to restore accessories from cache
 cmdSwitchPlatform.prototype.configureAccessory = function (accessory) {
   this.setService(accessory);
   this.accessories[accessory.context.name] = accessory;
 }
 
-// Method to setup accesories from config.json
 cmdSwitchPlatform.prototype.didFinishLaunching = function () {
-  // Add or update accessories defined in config.json
   for (var i in this.switches) this.addAccessory(this.switches[i]);
-
-  // Remove extra accessories in cache
   for (var name in this.accessories) {
     var accessory = this.accessories[name];
     if (!accessory.reachable) this.removeAccessory(accessory);
   }
 }
 
-// Method to add and update HomeKit accessories
 cmdSwitchPlatform.prototype.addAccessory = function (data) {
   this.log("Initializing platform accessory '" + data.name + "'...");
-
-  // Retrieve accessory from cache
   var accessory = this.accessories[data.name];
 
   if (!accessory) {
-    // Setup accessory as SWITCH (8) category.
     var uuid = UUIDGen.generate(data.name);
     accessory = new Accessory(data.name, uuid, 8);
-
-    // Setup HomeKit switch service
     accessory.addService(Service.Switch, data.name);
-
-    // New accessory is always reachable
-    accessory.reachable = true;
-
-    // Setup listeners for different switch events
     this.setService(accessory);
-
-    // Register new accessory in HomeKit
     this.api.registerPlatformAccessories("homebridge-cmdswitch2", "cmdSwitch2", [accessory]);
-
-    // Store accessory in cache
     this.accessories[data.name] = accessory;
   }
 
-  // Confirm variable type
+
   data.polling = data.polling === true;
   data.interval = parseInt(data.interval, 10) || 1;
-  data.timeout = parseInt(data.timeout, 10) || 1; 
+  data.timeout = parseInt(data.timeout, 10) || 1;
   if (data.manufacturer) data.manufacturer = data.manufacturer.toString();
   if (data.model) data.model = data.model.toString();
   if (data.serial) data.serial = data.serial.toString();
 
-  // Store and initialize variables into context
   var cache = accessory.context;
   cache.name = data.name;
   cache.on_cmd = data.on_cmd;
@@ -95,14 +74,14 @@ cmdSwitchPlatform.prototype.addAccessory = function (data) {
     if (data.off_cmd && !data.on_cmd) cache.state = true;
   }
 
-  // Retrieve initial state
   this.getInitState(accessory);
 
-  // Configure state polling
   if (data.polling && data.state_cmd) this.statePolling(data.name);
+
+  // Ensure accessory is marked as reachable
+  accessory.reachable = true;
 }
 
-// Method to remove accessories from HomeKit
 cmdSwitchPlatform.prototype.removeAccessory = function (accessory) {
   if (accessory) {
     var name = accessory.context.name;
@@ -112,7 +91,6 @@ cmdSwitchPlatform.prototype.removeAccessory = function (accessory) {
   }
 }
 
-// Method to setup listeners for different events
 cmdSwitchPlatform.prototype.setService = function (accessory) {
   accessory.getService(Service.Switch)
     .getCharacteristic(Characteristic.On)
@@ -122,44 +100,38 @@ cmdSwitchPlatform.prototype.setService = function (accessory) {
   accessory.on('identify', this.identify.bind(this, accessory.context));
 }
 
-// Method to retrieve initial state
 cmdSwitchPlatform.prototype.getInitState = function (accessory) {
   var manufacturer = accessory.context.manufacturer || "Default-Manufacturer";
   var model = accessory.context.model || "Default-Model";
   var serial = accessory.context.serial || "Default-SerialNumber";
 
-  // Update HomeKit accessory information
   accessory.getService(Service.AccessoryInformation)
     .setCharacteristic(Characteristic.Manufacturer, manufacturer)
     .setCharacteristic(Characteristic.Model, model)
     .setCharacteristic(Characteristic.SerialNumber, serial);
 
-  // Retrieve initial state if polling is disabled
   if (!accessory.context.polling) {
     accessory.getService(Service.Switch)
       .getCharacteristic(Characteristic.On)
       .getValue();
   }
 
-  // Configured accessory is reachable
-  accessory.updateReachability(true);
+  // Ensure accessory is marked as reachable
+  accessory.reachable = true;
+  this.api.emit('accessory-reachability-update', accessory, true);
 }
 
-// Method to determine current state
 cmdSwitchPlatform.prototype.getState = function (thisSwitch, callback) {
   var self = this;
 
-  // Return cached state if no state_cmd provided
   if (thisSwitch.state_cmd === undefined) {
     callback(null, thisSwitch.state);
     return;
   }
 
-  // Execute command to detect state
   exec(thisSwitch.state_cmd, function (error, stdout, stderr) {
     var state = error ? false : true;
 
-    // Error detection
     if (stderr) {
       self.log("Failed to determine " + thisSwitch.name + " state.");
       self.log(stderr);
@@ -169,16 +141,20 @@ cmdSwitchPlatform.prototype.getState = function (thisSwitch, callback) {
   });
 }
 
-// Method to determine current state
 cmdSwitchPlatform.prototype.statePolling = function (name) {
+  var self = this;
   var accessory = this.accessories[name];
+
+  if (!accessory || !accessory.context) {
+    this.log("Accessory or context is undefined for '" + name + "'.");
+    return;
+  }
+
   var thisSwitch = accessory.context;
 
-  // Clear polling
   clearTimeout(this.polling[name]);
 
   this.getState(thisSwitch, function (error, state) {
-    // Update state if there's no error
     if (!error && state !== thisSwitch.state) {
       thisSwitch.state = state;
       accessory.getService(Service.Switch)
@@ -187,31 +163,22 @@ cmdSwitchPlatform.prototype.statePolling = function (name) {
     }
   });
 
-  // Setup for next polling
   this.polling[name] = setTimeout(this.statePolling.bind(this, name), thisSwitch.interval * 1000);
 }
 
-// Method to determine current state
 cmdSwitchPlatform.prototype.getPowerState = function (thisSwitch, callback) {
   var self = this;
 
   if (thisSwitch.polling) {
-    // Get state directly from cache if polling is enabled
-    //this.log(thisSwitch.name + " is " + (thisSwitch.state ? "on." : "off."));
     callback(null, thisSwitch.state);
   } else {
-    // Check state if polling is disabled
     this.getState(thisSwitch, function (error, state) {
-      // Update state if command exists
       if (thisSwitch.state_cmd) thisSwitch.state = state;
-      //if (!error) self.log(thisSwitch.name + " is " + (thisSwitch.state ? "on." : "off."));
       callback(error, thisSwitch.state);
     });
   }
 }
 
-
-// Method to set state
 cmdSwitchPlatform.prototype.setPowerState = function (thisSwitch, state, callback) {
   var self = this;
 
@@ -219,19 +186,15 @@ cmdSwitchPlatform.prototype.setPowerState = function (thisSwitch, state, callbac
   var notCmd = state ? thisSwitch.off_cmd : thisSwitch.on_cmd;
   var tout = null;
 
-  // Execute command to set state
   exec(cmd, function (error, stdout, stderr) {
-    // Error detection
     if (error && (state !== thisSwitch.state)) {
       self.log("Failed to turn " + (state ? "on " : "off ") + thisSwitch.name);
       self.log(stderr);
     } else {
-      //if (cmd) self.log(thisSwitch.name + " is turned " + (state ? "on." : "off."));
       thisSwitch.state = state;
       error = null;
     }
 
-    // Restore switch after 1s if only one command exists
     if (!notCmd && !thisSwitch.state_cmd) {
       setTimeout(function () {
         self.accessories[thisSwitch.name].getService(Service.Switch)
@@ -239,21 +202,16 @@ cmdSwitchPlatform.prototype.setPowerState = function (thisSwitch, state, callbac
       }, 1000);
     }
 
-    if (tout) {
-      clearTimeout(tout);
-      callback(error);
-    }
+    callback(error);
   });
 
-  // Allow 1s to set state but otherwise assumes success
-  tout = setTimeout(function () {
-    tout = null;
-    self.log("Turning " + (state ? "on " : "off ") + thisSwitch.name + " took too long [" + thisSwitch.timeout + "s], assuming success." );
-    callback();
-  }, thisSwitch.timeout * 1000);
+  if (thisSwitch.state_cmd && thisSwitch.polling) {
+    tout = setTimeout(function () {
+      self.statePolling(thisSwitch.name);
+    }, thisSwitch.timeout * 1000);
+  }
 }
 
-// Method to handle identify request
 cmdSwitchPlatform.prototype.identify = function (thisSwitch, paired, callback) {
   this.log(thisSwitch.name + " identify requested!");
   callback();
